7e9952081cfecc6cbe2831d8f59ad24b6c11c111
Sean Liao <seanliao7@gmail.com>
2018-01-07 20:24:14 +0800

    1. First commit.

---
+++ TaskExceptionHandlingDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskExceptionHandlingDemo
+{
+    internal class Program
+    {
+        private static void CurrentDomain_UnhandledException( object sender, UnhandledExceptionEventArgs e )
+        {
+            Console.WriteLine( e );
+        }
+
+        private static Task DoWork( )
+        {
+            return Task.Run( ( ) => throw new Exception( "DoWork failed." ) );
+        }
+
+        private static Task<int> DoWork2( )
+        {
+            return Task.Run( ( ) =>
+            {
+                if ( false )
+                    return 3;
+                throw new Exception( "DoWork2 failed." );
+            } );
+        }
+
+        private static void Main( string[ ] args )
+        {
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            var t = DoWork( );
+            //t.Wait( );
+            //var result = DoWork2( );
+            //result = null;
+            Thread.Sleep( 1000 );
+            GC.Collect( );
+            GC.WaitForPendingFinalizers( );
+
+            Console.ReadKey( );
+        }
+
+        private static void TaskScheduler_UnobservedTaskException( object sender, UnobservedTaskExceptionEventArgs e )
+        {
+            e.SetObserved( );
+            Console.WriteLine( e );
+        }
+    }
+}
---
+++ TaskExceptionHandlingDemo/TaskExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskExceptionHandlingDemo
+{
+    public static class TaskExtensions
+    {
+        public static Task FailFastOnException( this Task task )
+        {
+            task.ContinueWith( c => Environment.FailFast( "Task faulted", c.Exception ),
+                TaskContinuationOptions.OnlyOnFaulted );
+            return task;
+        }
+
+        public static Task IgnoreExceptions( this Task task )
+        {
+            task.ContinueWith( c => Console.WriteLine( c.Exception ),
+                TaskContinuationOptions.OnlyOnFaulted );
+            return task;
+        }
+    }
+}
